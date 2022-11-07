2b110f6a5b97f5732e73dccaf8843055dc6183fb
RjCor <93812563+RjCor@users.noreply.github.com>
2022-11-06 22:22:45 -0500

    health sound

--- Assets/Scripts/Powerups/HealthPower.cs
+++ Assets/Scripts/Powerups/HealthPower.cs
@@ -6,6 +6,8 @@ public class HealthPower : MonoBehaviour
 {
     public int world;
     public int powerNo;
+    public AudioSource source;
+    public AudioClip heal;
 
     private void OnEnable()
     {
@@ -13,6 +15,12 @@ public class HealthPower : MonoBehaviour
         {
            gameObject.SetActive(false);
         }
+    }
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        heal = GetComponent<AudioClip>();
     }
 
     void Update()
@@ -25,6 +33,8 @@ public class HealthPower : MonoBehaviour
         GameObject obj = collision.gameObject;
         if (obj.CompareTag("Player"))
         {
+            heal = source.clip;
+            source.PlayOneShot(heal);
             PlayerHealth healthStats = obj.GetComponent<PlayerHealth>();
             ++healthStats.maxHealth;
             healthStats.health = healthStats.maxHealth;
